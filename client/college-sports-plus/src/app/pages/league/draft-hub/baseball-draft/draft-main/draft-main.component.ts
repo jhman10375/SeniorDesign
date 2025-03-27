64406145da431ecea3c6d16f24fb87f9f8efa8cb
jhman10375 <jordanherman1@gmail.com>
2025-03-27 01:13:51 -0400

    interim draft commit

---
+++ client/college-sports-plus/src/app/pages/league/draft-hub/baseball-draft/draft-main/draft-main.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+
+import { LeagueAthleteModel } from '../../../../../shared/models/league-athlete.model';
+import { PlayerFilterBase } from '../../../player-search/player-filter/player-filter.base.component';
+import { PlayerFilterComponent } from '../../../player-search/player-filter/player-filter/player-filter.component';
+import { PlayerSortComponent } from '../../../player-search/player-filter/player-sort/player-sort.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    CommonModule,
+    OverlayPanelModule,
+    PlayerSortComponent,
+    PlayerFilterComponent,
+  ],
+  selector: 'draft-main',
+  styleUrls: ['draft-main.component.scss'],
+  templateUrl: 'draft-main.component.html',
+})
+export class DraftMainComponent extends PlayerFilterBase implements OnInit {
+  // @Input() override set athletes(v: Array<LeagueAthleteModel>) {
+  //   if (v) {
+  //     this.playersReadonly = v;
+  //   }
+  // }
+
+  @Input() queue: Array<LeagueAthleteModel> = [];
+
+  @Input() currentlyPicking: boolean = false;
+
+  @Input() endDraft: boolean = false;
+
+  @Output() addToQueueEmitter = new EventEmitter<LeagueAthleteModel>();
+
+  @Output() addToRosterEmitter = new EventEmitter<LeagueAthleteModel>();
+
+  @Output() dialogPlayerEmitter = new EventEmitter<LeagueAthleteModel>();
+
+  @Output() searchEmitter = new EventEmitter<void>();
+
+  constructor() {
+    super();
+  }
+
+  override ngOnInit() {}
+
+  addToQueue(athlete: LeagueAthleteModel): void {
+    this.addToQueueEmitter.emit(athlete);
+  }
+
+  addToRoster(athlete: LeagueAthleteModel): void {
+    this.addToRosterEmitter.emit(athlete);
+  }
+
+  onPlayerSelected(player: LeagueAthleteModel): void {
+    this.dialogPlayerEmitter.emit(player);
+  }
+
+  onSearch(): void {
+    this.searchEmitter.emit();
+  }
+
+  inQueue(athlete: LeagueAthleteModel): boolean {
+    if (this.queue.find((x) => x.AthleteID === athlete.AthleteID)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- client/college-sports-plus/src/app/pages/league/draft/models/sport-player/football.model.ts
+++
@@ -1,7 +0,0 @@
-import { LeagueAthleteModel } from '../../../../../shared/models/league-athlete.model';
-import { FootballPlayerStatsModel } from '../../../../../shared/models/stats/football-player-stats.model';
-
-export class FootballDraftPlayerModel {
-  Athlete: LeagueAthleteModel;
-  Stats: FootballPlayerStatsModel;
-}
